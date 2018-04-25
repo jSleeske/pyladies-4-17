6e18bff73d68910499cb0f39bd082e6d679cfe10
jsleeske <jsleeske@gmail.com>
2018-04-25 16:54:38 +0200

    Expanded note about objects

--- 02-OOP/02.02-programowanie_obiektowe/20-everything-is-an-object.py
+++ 02-OOP/02.02-programowanie_obiektowe/20-everything-is-an-object.py
@@ -12,7 +12,7 @@ x.isalpha()
 #
 
 '''
-W Pythonie wszystkie klasy niejawnie dziedziczą po klasie object. Klasa object 
+W Pythonie wszystkie klasy niejawnie dziedziczą po klasie object. Klasa object
 jest więc klasą bazową wszystkich innych klas.
 '''
 o = object()
@@ -21,3 +21,53 @@ o = object()
 Klasa object definiuje wspólne metody dla wszystkich dziedziczących klas, m. in. 
 metodę __init__() oraz metodę __str__()
 '''
+
+
+'''
+W Pythonie funkcje też są obiektami, posiadają atrybuty i możemy je przypisywać
+do zmiennych.
+'''
+
+
+def greet(name):
+    '''
+    To jest tzw docstring funkcji
+    '''
+    print(f'Hello {name}!')
+
+
+'''
+Przy uruchamianiu kodu w miejscu definicji funkcji zostanie
+stworzony obiekt funkcji, jego nazwą będzie nazwa funkcji, np.
+w tym przypadku będzie będzie on dostepny pod zmienną greet
+'''
+
+'''
+Atrybut __doc__ przechowuje w sobie docstring funkcji (jeśli
+go zdefiniujemy).
+'''
+print(greet.__doc__)  # To jest tzw docstring funkcji
+
+'''
+Dodając nawiasy okrągłe wywołujemy funkcję!  
+'''
+print(greet('Jan'))  # Hello Jan
+
+say_hello = greet
+'''
+say_hello wskazuje teraz na ten sam obiekt funkcji!
+'''
+
+say_hello('PyLadies')  # ?
+
+'''
+Funkcje możemy przekazywać do innych funkcji, tak samo jak
+każdy inny obiekt!
+'''
+
+
+def call_a_function_with_param(param, func):
+    func(param)
+
+
+call_a_function_with_param('John', greet)  # ?
